b5cd6e720694ecf4d764e8e43e398fa74fe6cd34
just4rafa <rafajust4@gmail.com>
2023-07-20 20:25:30 +0300

    Adding the file to the repository

---
+++ Vowel_finder.py
@@ -0,0 +1,30 @@
+
+#Aplicatie de gasit vocale in cuvinte
+
+nrVocale = 0
+#Scrie cuvantul pe care il doresti
+exe = input("Write the word/sentence you want: ")
+
+for cuv in exe:
+    if cuv == "a":
+        print("I found the vowel", cuv)
+        nrVocale += 1
+    elif cuv == "e":
+        print("I found the vowel", cuv)
+        nrVocale += 1
+    elif cuv == "i":
+        print("I found the vowel", cuv)
+        nrVocale += 1
+    elif cuv == "o":
+        print("I found the vowel", cuv)
+        nrVocale += 1
+    elif cuv == "u":
+        print("I found the vowel", cuv)
+        nrVocale += 1
+    else:
+        print("The letter is not a vowel")
+
+print("The total number of vowels is", nrVocale)
+    
+        
+    
